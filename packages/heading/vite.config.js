2514ee6f5284522c004a282d7a28ba7a526354ae
fantasticit <fantasticit@163.com>
2024-05-18 15:16:07 +0800

    feat: extension-heading

---
+++ packages/heading/vite.config.js
@@ -0,0 +1,32 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import compress from "vite-plugin-compression";
+import dts from "vite-plugin-dts";
+
+const FileName = {
+  es: "index.js",
+  cjs: "index.cjs",
+  umd: "index.umd.js",
+};
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: "./src/index.ts",
+      formats: ["es", "cjs", "umd"],
+      fileName: (format) => FileName[format],
+      name: "SailkitHeading",
+    },
+    rollupOptions: {
+      external: ["@sailkit/core"],
+      output: {
+        globals: {
+          "@sailkit/core": "Sailkit",
+        },
+      },
+    },
+  },
+  resolve: { alias: { src: resolve("src/") } },
+  plugins: [dts({ rollupTypes: true }), compress()],
+});
